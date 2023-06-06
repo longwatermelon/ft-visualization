cf4a694cf60c6fd307ea8f1d04d0e3f29269aab5
longwatermelon <longwatermelon1@gmail.com>
2023-06-06 13:17:55 -0700

    window title

--- src/main.rs
+++ src/main.rs
@@ -101,6 +101,7 @@ fn window_conf() -> Conf {
         window_resizable: false,
         window_width: SCRW as i32,
         window_height: SCRH as i32,
+        window_title: String::from("Fourier transform visual"),
         ..Default::default()
     }
 }
